3c23f7120728128fc545c4e1d4c5bac1850c5611
Nick Lawler <lawler145@gmail.com>
2015-09-22 13:41:58 -0700

    add confirmationBox, skeleton of last 3 actions

--- lessons-ui/StripeCheckout.elm
+++ lessons-ui/StripeCheckout.elm
@@ -4,7 +4,7 @@ import StartApp
 import Html exposing (Html, text, div, button)
 import Html.Events exposing (onClick, onWithOptions, Options)
 import Html.Attributes
-import Json.Decode exposing (value)
+import Json.Decode exposing (Decoder, Value, value)
 import Effects exposing (Effects)
 import Task exposing (Task, andThen, succeed)
 import Debug
@@ -12,6 +12,11 @@ import Debug
 type alias Model =
     { key : PublishableKey
     , info : FormInfo
+    , confirmation : Maybe (Result ServerResponse String)
+    }
+
+type alias ServerResponse =
+    { chargeID : String
     }
 
 type alias PublishableKey = String
@@ -41,6 +46,7 @@ init pk =
     (
       { key = pk
       , info = emptyInfo
+      , confirmation = Nothing
       }
     , Effects.task (succeed Configure)
     )
@@ -71,16 +77,18 @@ noFx model =
     (model, Effects.none)
 
 
+
+
+
 type Action
     = NoOp
     | Configure -- sent in initialization, sends pubkey to js
     | Open -- sent by user to open the modal, sends other params to js, which then calls open, also sets token callback
     | TokenRecieved (Maybe (Token,Int)) -- sent by js token callback
-    -- | SubmitToServer Token -- sent by user, can send user defined Json Body
-    -- | RecieveFromServer Json.Decode.Value Json.Decode.Decoder a
-    -- | Close -- sent by Recieve to close the modal via port to js
-    | Amount Int -- set the amount
-    | Description String
+    | SubmitToServer (Token,Int) -- sent by user, can send user defined Json Body
+    | RecieveFromServer (Maybe ServerResponse)
+    | Close -- sent by Recieve to close the modal via port to js
+    | Choose Int String -- user supplied product parameters
 
 finish : a -> Task x Action
 finish = always <| succeed NoOp
@@ -106,36 +114,31 @@ update address model =
             )
         TokenRecieved maybeToken ->
             case maybeToken of
-                Just (token,amount) ->
-                    let log = Debug.log "Here ya go: " (token,amount)
-                    in
-                        noFx model
+                Just pair ->
+                        ( model
+                        , Effects.task (succeed (SubmitToServer pair) )
+                        )
                 Nothing ->
                     noFx model
-        Amount newAmt ->
+        SubmitToServer pair ->
+            Debug.crash "Not yet implemented SubmitToServer"
+        RecieveFromServer maybeResult ->
+            case maybeResult of
+                Just result ->
+                    Debug.crash "Not yet implemented RecieveFromServer Just"
+                Nothing ->
+                    Debug.crash "Not yet implemented RecieveFromServer Nothing"
+        Close -> -- no need to actually call this in the main flow, form closes after submit, could listen on a key
+            Debug.crash "Not yet implemented Close"
+        Choose newAmt newDes ->
             let info = model.info
             in
                 noFx { model |
-                        info <- { info | amount <- Just newAmt}
-                     }
-        Description newDes ->
-            let info = model.info
-            in
-                noFx { model |
-                        info <- { info | description <- Just newDes}
+                        info <- { info | amount <- Just newAmt
+                                , description <- Just newDes
+                                }
                      }
 
-
-
-
-{-|
-    1. When page is loaded, load stripejs
-    2. As soon as possible, set the pkey and send a configure js (might have to Configure on load if errors)
-    3. onClick of the button, send an Open, which somehow talks to a port to open with the passed in options in js
-       this also sets the "token" callback to send the token over the another port
-    4. When the
-
-|-}
 
 configMailbox : Signal.Mailbox PublishableKey
 configMailbox =
@@ -147,22 +150,38 @@ openMailbox =
     Signal.mailbox emptyInfo
 
 
-
 view : Signal.Address Action -> Model -> Html
 view address model =
-    div []
-        [ button [onClick address (Amount 5)] [text "Set 5"]
-        , button [onClick address (Amount 10)] [text "Set 10"]
-        , button [onClick address (Amount 0)] [text "Set 0"]
-        , button [onClick address Configure] [text "Configure"]
-        , button [onClick address Open] [text "Pay with Stripe"] -- may have to prevent default
-        , text ( "Current Amount: " ++
-                    (  model.info.amount
-                    |> Maybe.withDefault 0
-                    |> toString
-                    )
-               )
-        ]
+        div []
+            [ button [onClick address (Choose 2500 "1 hour")] [text "1 hour"]
+            , button [onClick address (Choose 4500 "2 hours")] [text "2 hours"]
+            , button [onClick address (Choose 7000 "3 hours")] [text "3 hours"]
+            , button [onClick address Open] [text "Checkout"] -- may have to prevent default
+            , text ( "Current Amount: " ++
+                        ( model.info.amount
+                            |> Maybe.withDefault 0
+                            |> toString
+                        )
+                   )
+            , confirmationBox model.confirmation
+            ]
+
+
+confirmationBox : Maybe (Result ServerResponse String) -> Html
+confirmationBox maybeResult =
+    case maybeResult of
+        Just result ->
+            case result of
+                Ok response ->
+                    div []
+                        [ text "You're all set! Save your charge ID: " ++ (toString result.chargeID)]
+                Err msg ->
+                    div [] [text "Yikes! Your charge didn't go through, contact Nick asap"]
+        Nothing ->
+            div [] []
+
+
+
 
 app =
     StartApp.start
@@ -180,6 +199,7 @@ port configureStripe = Signal.dropRepeats configMailbox.signal
 port openStripe : Signal FormInfo
 port openStripe = openMailbox.signal
 
+-- TODO add a Nothing case
 incomingToken' : Signal Action
 incomingToken' = Signal.map (\pair -> TokenRecieved (Just pair) ) incomingToken
 
