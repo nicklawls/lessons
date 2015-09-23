abee6240ee35250bcb941d785a4376b1b9b07bbc
Nick Lawler <lawler145@gmail.com>
2015-09-23 01:13:41 -0700

    remove dummy clear button, clean up server

--- lessons-api/app/Main.hs
+++ lessons-api/app/Main.hs
@@ -1,11 +1,34 @@
 module Main where
 
 import Lib
+import Network.Wai
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.Cors
+import Network.Wai.Middleware.RequestLogger
 
+
+-- pull out Live/Test split into main, pick a logger
 
 
 main :: IO ()
 main = do
-    print "Lessons running on port 8081 " 
-    run 8081 app
+    print "Lessons running on port 8081 "
+    run 8081 $ logStdoutDev (checkoutCors app)
+
+
+checkoutCors :: Middleware
+checkoutCors = cors (const $ Just checkoutCorsResourcePolicy)
+
+
+checkoutCorsResourcePolicy :: CorsResourcePolicy
+checkoutCorsResourcePolicy =
+    CorsResourcePolicy
+        { corsOrigins = Nothing
+        , corsMethods = simpleMethods
+        , corsRequestHeaders = simpleHeaders -- adds "Content-Type" to defaults
+        , corsExposedHeaders = Nothing
+        , corsMaxAge = Nothing
+        , corsVaryOrigin = False
+        , corsRequireOrigin = False
+        , corsIgnoreFailures = False
+        }
